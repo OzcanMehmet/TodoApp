31ac6da031113f9aa0d9991ee679594f3bff3634
Meozcan <ozcan.mehmet1994@gmail.com>
2020-08-17 14:13:19 +0300

    Proje dosyası ekle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ToDo.Service;
+using ToDo.Web.Models;
+
+namespace ToDo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private ITodoService _todoService;
+        public HomeController(ITodoService todoService)
+        {
+            _todoService = todoService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_todoService.GetAll());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Entity.ToDo entity)
+        {
+            if(ModelState.IsValid)
+            {
+                _todoService.Insert(entity);
+                Console.WriteLine("xyz");
+                return RedirectToAction("Index");
+            }
+            return View(entity);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var todo = _todoService.FindById((int)id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            _todoService.Delete(todo);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            Entity.ToDo toDo = _todoService.FindById(id);
+            if (toDo == null)
+                return NotFound();
+            return View(toDo);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Entity.ToDo entity)
+        {
+            Entity.ToDo toDo = _todoService.FindById(entity.Id);
+            if (toDo == null)
+                return NotFound();
+            _todoService.Update(entity);
+            return RedirectToAction("Index");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Migrations/20200817063811_init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDo.Web.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    UpdateTime = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ExecuteTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDo");
+        }
+    }
+}
